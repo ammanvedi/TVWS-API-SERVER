07e5e6b33019ead57dc2dd22d757f888679a51b6
Amman Vedi <amman.vedi@gmail.com>
2015-03-02 17:03:38 +0000

    remove print and switch path for server

--- Processing/Data_prep/GenerateChannelReadings.py
+++ Processing/Data_prep/GenerateChannelReadings.py
@@ -118,8 +118,8 @@ class ReadingsParser:
 		#ADD TRACK RECORD HERE!!
 		self.MIN_POWER = minimumpower
 		self.MIN_DISTANCE = minimumdistance
-		#self.RegionID = RegionSelector("/srv/TVWSAPI/TVWS-API-SERVER" + "/Processing/Data_prep/meta/ITURegionCountries.json", "/srv/TVWSAPI/TVWS-API-SERVER" + "/Processing/Data_prep/meta/countries.geo.json", "/srv/TVWSAPI/TVWS-API-SERVER" + "/Processing/Data_prep/meta/channelallocations.json")
-		self.RegionID = RegionSelector("/Users/ammanvedi/Documents/cs/year3/TVWhiteSpaceProject/PROJECT_CODE_FINAL/TVWS/Server-Python" + "/Processing/Data_prep/meta/ITURegionCountries.json", "/Users/ammanvedi/Documents/cs/year3/TVWhiteSpaceProject/PROJECT_CODE_FINAL/TVWS/Server-Python" + "/Processing/Data_prep/meta/countries.geo.json", "/Users/ammanvedi/Documents/cs/year3/TVWhiteSpaceProject/PROJECT_CODE_FINAL/TVWS/Server-Python" + "/Processing/Data_prep/meta/channelallocations.json")
+		self.RegionID = RegionSelector("/srv/TVWSAPI/TVWS-API-SERVER" + "/Processing/Data_prep/meta/ITURegionCountries.json", "/srv/TVWSAPI/TVWS-API-SERVER" + "/Processing/Data_prep/meta/countries.geo.json", "/srv/TVWSAPI/TVWS-API-SERVER" + "/Processing/Data_prep/meta/channelallocations.json")
+		#self.RegionID = RegionSelector("/Users/ammanvedi/Documents/cs/year3/TVWhiteSpaceProject/PROJECT_CODE_FINAL/TVWS/Server-Python" + "/Processing/Data_prep/meta/ITURegionCountries.json", "/Users/ammanvedi/Documents/cs/year3/TVWhiteSpaceProject/PROJECT_CODE_FINAL/TVWS/Server-Python" + "/Processing/Data_prep/meta/countries.geo.json", "/Users/ammanvedi/Documents/cs/year3/TVWhiteSpaceProject/PROJECT_CODE_FINAL/TVWS/Server-Python" + "/Processing/Data_prep/meta/channelallocations.json")
 		self.N = Notifier(self.RegionID.getConnection())
 		self.TRACKID = tracker
 		self.N.addTrackRecord(user, tracker)
@@ -188,7 +188,6 @@ class ReadingsParser:
 			for val in freqsinrange:
 				combined += (spectrum[val])
 			combined = (combined/len(freqsinrange))
-			print combined
 			res.append(combined)
 		return res
 
