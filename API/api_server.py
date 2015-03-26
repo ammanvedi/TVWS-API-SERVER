ad92a3398dfa1c4dea1ebaf7fa8db389deb796c4
Amman Vedi <amman.vedi@gmail.com>
2015-03-26 06:08:07 +0000

    ssl patch

--- API/api_server.py
+++ API/api_server.py
@@ -14,6 +14,14 @@ import psycopg2
 from DatabaseHelper import DBHelper
 import json
 import ssl
+from functools import wraps
+def sslwrap(func):
+    @wraps(func)
+    def bar(*args, **kw):
+        kw['ssl_version'] = ssl.PROTOCOL_TLSv1
+        return func(*args, **kw)
+    return bar
+ssl.wrap_socket = sslwrap(ssl.wrap_socket)
 
 psqlHelper = DBHelper();
 
